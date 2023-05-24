5fe1784737728ef42204710feb98f9c77cd55f91
anthony <anthonyricharddasilvamartins2@gmail.com>
2023-05-24 20:04:45 -0300

    arquivo que retorna todos os pontos de coleta em analise.

--- PHP/depuracao.php
+++ PHP/depuracao.php
@@ -1,24 +1,13 @@
-<?php 
-    include 'config.php';
-    header("Access-Control-Allow-Origin: *");
+<?php
+include 'config.php';
+header("Access-Control-Allow-Origin: *");
 
-    $data = [];
-    try{
-        $sql = $conn->query('SELECT * FROM empresa');
-    
-        foreach($sql as $row){
-           // print_r($row);
-          $data[] = [
-             "cnpj" => $row[0],
-             "name" => $row[1],
-             "email" => $row[2],
-             "telefone" => $row[3]
-            ];
-        }
-
-    } catch(PDOException $e){
-        echo 'ERROR: ' . $e->getMessage();
-    }
-
+$sql = $conn->query('SELECT * FROM EMP_PONTO_DE_COLETA');
+$data = $sql->fetchAll(PDO::FETCH_ASSOC);
+if(count($data) == 0){
+    echo json_encode(["erro" => "User Not Found"]);
+}else{
     echo json_encode($data);
+    
+}
 ?>
