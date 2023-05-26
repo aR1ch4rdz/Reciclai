db23e5b71589957954c37d59604e8c83c21d0181
anthony <anthonyricharddasilvamartins2@gmail.com>
2023-05-26 10:50:53 -0300

    pagina com os pontos de coleta em analise

---
+++ VUE/Projeto/src/views/PontosDeColetaView.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { Suspense } from 'vue';
+import PontoDeColeta from '../components/PontoDeColeta.vue';
+import DashMenu from '../components/DashMenu.vue';
+</script>
+
+<template>
+  <div class="content-wrapper">
+    <DashMenu />
+    <div class="content">
+      <Suspense>
+        <PontoDeColeta />
+        <template #fallback>
+          Carregando Pontos de coleta...
+        </template>
+      </Suspense>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+div.content-wrapper {
+  position: relative;
+  display: flex;
+  grid-template-columns: 0.5fr 0.5fr;
+  width: 100%;
+  min-height: 90%;
+}
+div.content{
+  width: 100%;
+  min-height: 100%;
+  display: grid;
+  padding: 1em;
+  gap: 1em;
+  grid-template-columns: 1fr 1fr;
+  background-color: #ededed;
+}
+</style>
