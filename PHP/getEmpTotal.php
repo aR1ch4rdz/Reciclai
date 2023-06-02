5c5a65f693aae694a8c2b0ff4a825482ba9d1585
anthony <anthonyricharddasilvamartins2@gmail.com>
2023-06-01 21:09:05 -0300

    arquivo que retorna total de empresas cadastardas

--- PHP/getEmpTotal.php
+++ PHP/getEmpTotal.php
@@ -2,7 +2,7 @@
 require('config.php');
 header("Access-Control-Allow-Origin: *");
 
-$sql = $conn->query("SELECT COUNT(*) AS QTDE_EMPRESA FROM USR_EMPRESA;");
+$sql = $conn->query("SELECT COUNT(*) AS TOTAL FROM USR_EMPRESA;");
 $data = $sql->fetchAll(PDO::FETCH_ASSOC);
 
 echo json_encode($data);
