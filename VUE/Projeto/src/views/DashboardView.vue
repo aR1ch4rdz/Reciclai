f7235507596e4f5925150cc9b3fd03f5c644beaf
anthony <anthonyricharddasilvamartins2@gmail.com>
2023-06-01 21:13:11 -0300

    adicionado graph e totalCard ao dash board

--- VUE/Projeto/src/views/DashboardView.vue
+++ VUE/Projeto/src/views/DashboardView.vue
@@ -3,10 +3,10 @@ import { Suspense } from "vue";
 import router from "../router/index"
 import DashMenu from "../components/DashMenu.vue"
 import TotalCard from "../components/TotalCard.vue"
+import Graph from "../components/Graph.vue";
 function moveUser() {
 	router.push('/dashboard/pontos')
 }
-
 
 </script>
 
@@ -16,27 +16,30 @@ function moveUser() {
 		<Suspense>
 			<div class="dashinfo-wrapper">
 				<div class="dash-row">
-					<TotalCard />
-					<div @click="moveUser" class="card">
-						<h1>Pontos Pendentes</h1>
-						<p>20</p>
-					</div>
-					<div class="card">
-						<h1>Total De Empresas</h1>
-						<p>100</p>
-					</div>
+					<TotalCard icon="build" path="getEmpTotal.php" card="Total de empresa" />
+					<TotalCard @click="moveUser()" icon="map" path="getPDCTotal.php" card="Total de Pontos" />
+					<TotalCard icon="map" path="getEmpTotal.php" card="Pontos pendentes" />
+				</div>
+				<div class="graph-wrapper">
+					<Graph />
 				</div>
 			</div>
 		</Suspense>
-		</div>
+	</div>
 </template>
-
 
 <style scoped>
 .content-wrapper {
 	display: flex;
 	width: 100%;
 	height: 90%;
+}
+div.graph-wrapper{
+	text-align: center;
+	box-shadow: hsla(0, 0%, 0%, 0.184) 0px 0px 8px;
+	width: 100%;
+	height: 80%;
+	margin-top: 2%;
 }
 
 p {
@@ -48,8 +51,9 @@ p {
 	width: 100%;
 	height: 100%;
 	background-color: #ededed;
-	display: grid;
-	padding: 3em;
+	display: flex;
+	flex-direction: column;
+	padding: 2em;
 }
 
 .dash-row {
