b8ab52a874376b37e9dffa3b96d85c0be103412e
anthony <anthonyricharddasilvamartins2@gmail.com>
2023-06-01 21:10:34 -0300

    card de informações da dashboard

--- VUE/Projeto/src/components/TotalCard.vue
+++ VUE/Projeto/src/components/TotalCard.vue
@@ -1,20 +1,37 @@
 <script setup>
-const totalEmpresa = await fetch("http://localhost:8005/getEmpTotal.php")
+import { MapPinIcon, BuildingOfficeIcon } from '@heroicons/vue/24/outline';
+let props = defineProps({
+	path: '',
+	card:'',
+	icon: ''
+})
+const totalEmpresa = await fetch(`http://localhost:8005/${props.path}`)
 const total = await totalEmpresa.json()
 </script>
 
 <template>
     <div class="card">
-        <h1>Empresas Pendentes</h1>
-        <p>{{ total[0].QTDE_EMPRESA }}</p>
+		<div class="card-row">
+			<h1>{{ props.card }}</h1>
+			<BuildingOfficeIcon class="icon" v-if="props.icon == 'build'"/>
+			<MapPinIcon class="icon" v-if="props.icon == 'map'"/>
+		</div>
+        <div class="card-row">
+			<p>{{ total[0].TOTAL }}</p>
+		</div>
     </div>
 </template>
 
 <style scoped>
-
+.icon{
+	width: 36px;
+}
 div.card {
+	box-sizing: border-box;
 	background-color: white;
 	height: fit-content;
+	width: 100%;
+	border-radius: 0.2em;
 	padding: 1em;
 	border-bottom: 5px #cccccc solid;
 	transition: all ease-in-out 200ms;
@@ -25,6 +42,19 @@ div.card {
 div.card:hover {
 	transform: scale(1.02);
 	border-bottom: 5px var(--green) solid;
+}
+h1{
+	font-size: clamp(1em,1.5em,2em);
+
+}
+p{
+	font-weight: 700;
+	font-size: clamp(1em,1.5em,2em);
+}
+div.card-row{
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
 }
 
 
