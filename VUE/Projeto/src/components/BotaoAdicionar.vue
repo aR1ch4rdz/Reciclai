ab366f19196a60723df6c5fd734a7146a5047027
anthony <anthonyricharddasilvamartins2@gmail.com>
2023-06-15 20:35:35 -0300

    criando componente

---
+++ VUE/Projeto/src/components/BotaoAdicionar.vue
@@ -0,0 +1,47 @@
+<script setup>
+import { PlusCircleIcon, MapPinIcon } from '@heroicons/vue/24/outline';
+
+</script>
+
+<template>
+  <div class="button-wrapper">
+    <span class="icon-wrapper">
+      <PlusCircleIcon class="icon" />
+    </span>
+    <span class="text-wrapper">
+      Adicionar Novo Ponto
+    </span>
+  </div>
+</template>
+
+<style scoped>
+
+.button-wrapper {
+  user-select: none;
+  display: inline-flex;
+  height: 5%;
+  background-color: var(--green);
+  overflow: hidden;
+  border-radius: 5px;
+  font-size: 1em;
+  font-weight: 500;
+  cursor: pointer;
+}
+
+.text-wrapper {
+  display: inline-flex;
+  align-items: center;
+  padding: 0 0.5em;
+  color: white;
+}
+
+.icon {
+  color: white;
+  height: 100%;
+}
+
+.icon-wrapper {
+  height: 100%;
+  padding: 0 0.5em;
+}
+</style>
--- VUE/Projeto/src/views/UserPage.vue
+++ VUE/Projeto/src/views/UserPage.vue
@@ -1,23 +1,79 @@
 <script setup>
 import { logedUser } from '../assets/variaveis.js';
-import { reactive } from 'vue';
+import { ref, onMounted } from 'vue';
+import  BotaoAdicionar  from '../components/BotaoAdicionar.vue'
+const hiddenForm = ref(true);
 
-
+const formRef = ref(null);
+function showForm() {
+  hiddenForm.value = !hiddenForm.value;
+  if (!hiddenForm.value) {
+    formRef.value.style.display = "flex";
+  } else {
+    formRef.value.style.display = "none";
+  }
+}
 
 </script>
 
 <template>
   <div class="container">
-    
+        <BotaoAdicionar @click="showForm()" />
+
+    <form ref="formRef">
+      <label for="">adwad</label>
+      <input type="text" name="" id="">
+
+      <label for="">adwad</label>
+      <input type="text" name="" id="">
+
+      <label for="">adwad</label>
+      <input type="text" name="" id="">
+
+    </form>
+
   </div>
 </template>
 
 <style scoped>
+form {
+  display: none;
+  flex-direction: column;
+}
+
 div.container {
   display: flex;
+  justify-content: center;
+  align-items: center;
   width: 100%;
   height: 100%;
 }
 
+.button-wrapper {
+  user-select: none;
+  display: inline-flex;
+  height: 5%;
+  background-color: var(--green);
+  overflow: hidden;
+  border-radius: 5px;
+  font-size: 1em;
+  font-weight: 500;
+  cursor: pointer;
+}
+
+.text-wrapper{
+  display: inline-flex;
+  align-items: center;
+  padding: 0 0.5em;
+  color: white;  
+}
+.icon{
+  color: white;
+  height: 100%;
+}
+.icon-wrapper{
+  height: 100%;
+  padding: 0 0.5em;
+}
 
 </style>
